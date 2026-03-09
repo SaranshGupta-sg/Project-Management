400d17e958982d5a4ab2091d2881b191e6ad02a3
SaranshGupta-sg <saransh.g2911@gmail.com>
2026-03-09 22:32:04 +0530

    new

---
+++ server/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import { clerkMiddleware } from '@clerk/express'
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(clerkMiddleware())
+
+app.get("/", (req, res) => res.send("Server is Live!"));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
