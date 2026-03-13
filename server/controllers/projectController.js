0a728ca1b2f61c76a3ac2cf590c4c9f260a2b206
Saransh Gupta <saransh.g2911@gmail.com>
2026-03-14 00:45:01 +0530

    Add createProject function in projectController
    Implemented createProject function to handle project creation with user authentication and workspace validation.

--- server/controllers/projectController.js
+++ server/controllers/projectController.js
@@ -1,1 +1,47 @@
-// hii
+import prisma from "../configs/prisma.js";
+
+// Create project
+export const createProject = async (req, res) => {
+  try {
+    const { userId } = await req.auth();
+    const {
+      workspaceId,
+      description,
+      name,
+      status,
+      start_date,
+      end_name,
+      team_members,
+      team_lead,
+      progress,
+      priority,
+    } = req.body;
+
+    // Check if user has admin role for workspace
+    const workspace = await prisma.workspace.findUnique({
+      where: { id: workspaceId },
+      include: { members: { include: { user: true } } },
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.code || error.message });
+  }
+};
+
+// Update project
+export const updateProject = async (req, res) => {
+  try {
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.code || error.message });
+  }
+};
+
+// Add Member to project
+export const addMember = async (req, res) => {
+  try {
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.code || error.message });
+  }
+};
