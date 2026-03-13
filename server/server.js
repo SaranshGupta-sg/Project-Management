8f92d5d409bfd920822e72cc87a3102a1f40a5a3
Saransh Gupta <saransh.g2911@gmail.com>
2026-03-13 22:28:33 +0530

    Integrate workspace routes into server
    Added workspaceRouter to handle workspace-related routes.

--- server/server.js
+++ server/server.js
@@ -1,19 +1,23 @@
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
-import { clerkMiddleware } from '@clerk/express'
+import { clerkMiddleware } from "@clerk/express";
 import { serve } from "inngest/express";
-import { inngest, functions } from "./inngest/index.js"
+import { inngest, functions } from "./inngest/index.js";
+import workspaceRouter from "./routes/workspaceRoutes.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use(clerkMiddleware())
+app.use(clerkMiddleware());
 
 app.get("/", (req, res) => res.send("Server is Live!"));
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
+
+// Routes
+app.use("/api/workspaces", workspaceRouter);
 
 const PORT = process.env.PORT || 3000;
 
