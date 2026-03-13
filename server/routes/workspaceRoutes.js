34941f634c47bd9b616a110c7919d42c85f4f145
Saransh Gupta <saransh.g2911@gmail.com>
2026-03-13 22:25:15 +0530

    Refactor workspaceRoutes to use express router

--- server/routes/workspaceRoutes.js
+++ server/routes/workspaceRoutes.js
@@ -1,1 +1,12 @@
-// Hii
+import express from "express";
+import {
+  addMember,
+  getUserWorkspaces,
+} from "../controllers/workspaceController.js";
+
+const workspaceRouter = express.Router();
+
+workspaceRouter.get("/", getUserWorkspaces);
+workspaceRouter.post("/add-member", addMember);
+
+export default workspaceRouter;
