ea2d0d7e7f01bfe4df32b13d002e9edac4d3edc2
Saransh Gupta <saransh.g2911@gmail.com>
2026-03-15 00:12:58 +0530

    Refactor comment routes to use Express router

--- server/routes/commentRoutes.js
+++ server/routes/commentRoutes.js
@@ -1,1 +1,12 @@
-// hi
+import express from "express";
+import {
+  addComment,
+  getTaskComments,
+} from "../controllers/commentController.js";
+
+const commentRouter = express.Router();
+
+commentRouter.post("/", addComment);
+commentRouter.get("/:taskId", getTaskComments);
+
+export default commentRouter;
