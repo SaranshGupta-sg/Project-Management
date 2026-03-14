0f771084cb9c2b4f05bf72ad67a32c94df902fbf
Saransh Gupta <saransh.g2911@gmail.com>
2026-03-14 22:22:52 +0530

    Set up task routes for CRUD operations

--- server/routes/taskRoutes.js
+++ server/routes/taskRoutes.js
@@ -1,1 +1,14 @@
-// hii
+import express from "express";
+import {
+  createTask,
+  deleteTask,
+  updateTask,
+} from "../controllers/taskContoller.js";
+
+const taskRouter = express.Router();
+
+taskRouter.post("/", createTask);
+taskRouter.put("/:id", updateTask);
+taskRouter.post("/delete", deleteTask);
+
+export default taskRouter;
